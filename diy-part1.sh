e0d1df5d93b8bbbac52c9a877e48ef278efb3894
zhongfab <148003017+zhongfab@users.noreply.github.com>
2024-04-04 17:19:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone https://github.com/zhongfab/mycum-package package/tomato-pac
+git clone https://github.com/zhongfab/packages-zhongfab package/tomato-extend
